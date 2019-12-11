3124b47ff9001cc9bff7db3b88287b8351c82277
孙鸿斌 <sunhb17@mails.tsinghua.edu.cn>
2019-12-11 17:37:39 +0800

    m

--- checksum.cpp
+++ checksum.cpp
@@ -1,5 +1,6 @@
 #include <stdint.h>
 #include <stdlib.h>
+#include <cstdio>
 
 /**
  * @brief 进行 IP 头的校验和的验证
@@ -8,27 +9,23 @@
  * @return 校验和无误则返回 true ，有误则返回 false
  */
 bool validateIPChecksum(uint8_t *packet, size_t len) {
-	unsigned int ans = (packet[10]<<8) + packet[11];
-	unsigned short check_sum = 0;
+	
 	unsigned int checksum = 0;
-
+	unsigned short cksum = 0;
+	unsigned int answer = (packet[10] << 8) + packet[11];
 	packet[10] = 0;
 	packet[11] = 0;
-
-	int ip_len = (packet[0] << 2) % 64;
-
-	for(int i=0;i<ip_len;i+=2){
+	int iplen = (packet[0] % 16) << 2;
+	for(int i = 0; i < iplen; i+=2){
 		checksum += ((packet[i] << 8) + packet[i+1]);
 	}
-
-	while(checksum>>16!=0){
-		checksum = (checksum & 0xffff) + (checksum >> 16);
-	}
-
-	check_sum = ~checksum;
-	if(check_sum==ans)
+	checksum = (checksum & 0xffff) + (checksum >> 16);
+	checksum += (checksum >> 16);
+	cksum = ~checksum;
+	if(cksum == answer){
 		return true;
-	else
+	}
+	else {
 		return false;
-
+	}
 }
